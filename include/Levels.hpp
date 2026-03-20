c6a0f9c9e6bddc4d654167206c572bbe19d13146
cheeserabbitNaOH <perry941029@gmail.com>
2026-03-20 11:13:03 +0800

    Levels enum class created

---
+++ include/Levels.hpp
@@ -0,0 +1,13 @@
+//
+// Created by cheese on 2026/3/20.
+//
+
+#ifndef JUST_SHAPES_AND_BEATS_LEVELS_HPP
+#define JUST_SHAPES_AND_BEATS_LEVELS_HPP
+
+enum class Levels {
+    Main,
+    Chronos
+};
+
+#endif //JUST_SHAPES_AND_BEATS_LEVELS_HPP
