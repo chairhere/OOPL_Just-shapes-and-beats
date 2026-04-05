6ff04a06d826e3464cf00a5c479503fdd50254c2
cheeserabbitNaOH <perry941029@gmail.com>
2026-04-05 22:07:57 +0800

    新增TextObject(文字物件)

---
+++ include/TextObject.hpp
@@ -0,0 +1,19 @@
+//
+// Created by cheese on 2026/4/5.
+//
+
+#ifndef JUST_SHAPES_AND_BEATS_TEXTOBJECT_HPP
+#define JUST_SHAPES_AND_BEATS_TEXTOBJECT_HPP
+#include "Util/GameObject.hpp"
+#include "Util/Text.hpp"
+
+class TextObject : public Util::GameObject {
+public:
+    TextObject(const std::string& fontPath, int fontSize, const std::string& text, const Util::Color& color) {
+        m_Drawable = std::make_shared<Util::Text>(fontPath, fontSize, text, color);
+    }
+
+    void SetText(const std::string& text);
+};
+
+#endif //JUST_SHAPES_AND_BEATS_TEXTOBJECT_HPP
---
+++ src/TextObject.cpp
@@ -0,0 +1,11 @@
+//
+// Created by cheese on 2026/4/5.
+//
+
+#include "TextObject.hpp"
+
+void TextObject::SetText(const std::string &text) {
+    if (auto textDrawable = std::dynamic_pointer_cast<Util::Text>(m_Drawable)) {
+        textDrawable->SetText(text);
+    }
+}
