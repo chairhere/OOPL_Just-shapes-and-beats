7e2bbc7ce71c12719befac89e5c5b713f8ff588a
cheeserabbitNaOH <perry941029@gmail.com>
2026-04-05 22:38:12 +0800

    新增ImageObject

---
+++ include/ImageObject.hpp
@@ -0,0 +1,18 @@
+//
+// Created by cheese on 2026/4/5.
+//
+
+#ifndef JUST_SHAPES_AND_BEATS_IMAGEOBJECT_HPP
+#define JUST_SHAPES_AND_BEATS_IMAGEOBJECT_HPP
+#include "Util/GameObject.hpp"
+#include "Util/Image.hpp"
+
+class ImageObject : public Util::GameObject {
+    ImageObject(const std::string& imagePath) {
+        m_Drawable = std::make_shared<Util::Image>(imagePath);
+    }
+
+    void SetImage(const std::string& imagePath);
+};
+
+#endif //JUST_SHAPES_AND_BEATS_IMAGEOBJECT_HPP
