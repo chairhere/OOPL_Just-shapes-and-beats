4ebccac695691a00dbbf9254a5b5a5d557c57371
cheeserabbitNaOH <perry941029@gmail.com>
2026-04-06 21:16:58 +0800

    SongList改為以enum搜索

--- include/SongList.hpp
+++ include/SongList.hpp
@@ -4,16 +4,33 @@
 
 #ifndef JUST_SHAPES_AND_BEATS_SONGLIST_HPP
 #define JUST_SHAPES_AND_BEATS_SONGLIST_HPP
+#include <unordered_map>
 #include <vector>
 
+#include "Levels.hpp"
 #include "SongData.hpp"
 #include "SongsBPM.hpp"
+#include "Util/Logger.hpp"
 
 class SongList {
 public:
-    inline static const std::vector<SongData> SongDatas = {
-        SongData{"Chronos", "Danimal Cannon & Zef", SongsBPM::Chronos, "../Resources/Audio/Chronos.mp3"}
+    static const std::unordered_map<Levels, SongData>& GetAllSong() {
+        static const std::unordered_map<Levels, SongData> s_Songs = {
+            {Levels::Chronos, SongData{"Chronos", "Danimal Cannon & Zef", SongsBPM::Chronos, "../Resources/Audio/Chronos.mp3"}}
+        };
+        return s_Songs;
     };
+
+    static const SongData& GetSongByName(const Levels songName) {
+        const auto& songs = GetAllSong();
+
+        if (songs.find(songName) != songs.end()) {
+            return songs.at(songName);
+        }else {
+            LOG_ERROR("Song {} NOT found", to_string(songName));
+            throw std::invalid_argument("Song not found");
+        }
+    }
 };
 
 #endif //JUST_SHAPES_AND_BEATS_SONGLIST_HPP
